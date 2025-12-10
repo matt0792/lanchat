2c76b554b53880c0715f1c94c60e923acae06995
Matthew Thompson <matthew.thompson@velocitytrade.com>
2025-12-10 17:43:38 +0200

    add sdk

---
+++ sdk/lanchat.go
@@ -0,0 +1,75 @@
+package sdk
+
+import (
+	"context"
+
+	"github.com/matt0792/lanchat/internal/app"
+)
+
+type Lanchat struct {
+	app     *app.App
+	handler EventHandler
+	logger  Logger
+}
+
+func New(ctx context.Context, nickname string, handler EventHandler, logger Logger) (*Lanchat, error) {
+	app, err := app.NewApp(ctx, nickname)
+	if err != nil {
+		return nil, err
+	}
+
+	if handler == nil {
+		handler = &defaultEventHandler{}
+	}
+
+	if logger == nil {
+		logger = &defaultLogger{}
+	}
+
+	lc := &Lanchat{
+		app:     app,
+		handler: handler,
+		logger:  logger,
+	}
+
+	return lc, nil
+}
+
+func (l *Lanchat) JoinRoom(roomName, password string) error {
+	return l.app.JoinRoom(roomName, password)
+}
+
+func (l *Lanchat) LeaveRoom() error {
+	return l.app.LeaveRoom()
+}
+
+func (l *Lanchat) GetRoomList() []string {
+	return l.app.GetRoomList()
+}
+
+func (l *Lanchat) GetPeerList() []string {
+	return l.app.GetPeerList()
+}
+
+func (l *Lanchat) SendMessage(text string) error {
+	return l.app.SendMessage(text)
+}
+
+func (l *Lanchat) HandleEvents() {
+	for event := range l.app.GetEvents() {
+		switch event.Type {
+		case app.EventMessageRecv:
+			l.handler.HandleMessageRecv(convertChatMessage(event.Data.(*app.ChatMessage)))
+
+		case app.EventPeerJoined:
+			l.handler.HandlePeerJoined(convertPeerInfo(event.Data.(*app.PeerInfo)))
+
+		case app.EventRoomJoined:
+			l.handler.HandleRoomJoined(convertRoom(event.Data.(*app.Room)))
+		}
+	}
+}
+
+func (l *Lanchat) Close() error {
+	return l.app.Close()
+}
---
+++ sdk/types.go
@@ -0,0 +1,156 @@
+package sdk
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/matt0792/lanchat/internal/app"
+)
+
+type User struct {
+	Nickname string
+	Status   string
+}
+
+type PeerInfo struct {
+	ID       string
+	Nickname string
+	Status   string
+	LastSeen time.Time
+	Metadata map[string]string
+}
+
+type Room struct {
+	Name     string
+	Peers    []*PeerInfo
+	Messages []*ChatMessage
+}
+
+type ChatMessage struct {
+	ID        string
+	From      string
+	Nickname  string
+	Content   string
+	Timestamp time.Time
+	Type      MessageType
+}
+
+type MessageType string
+
+const (
+	MessageTypeText   MessageType = "text"
+	MessageTypeJoin   MessageType = "join"
+	MessageTypeLeave  MessageType = "leave"
+	MessageTypeTyping MessageType = "typing"
+	MessageTypeStatus MessageType = "status"
+)
+
+type EventType string
+
+const (
+	EventPeerJoined   EventType = "peer_joined"
+	EventPeerLeft     EventType = "peer_left"
+	EventMessageRecv  EventType = "message_received"
+	EventRoomJoined   EventType = "room_joined"
+	EventStatusChange EventType = "status_change"
+)
+
+func convertUser(u *app.User) *User {
+	if u == nil {
+		return nil
+	}
+	return &User{
+		Nickname: u.Nickname,
+		Status:   u.Status,
+	}
+}
+
+func convertPeerInfo(p *app.PeerInfo) *PeerInfo {
+	if p == nil {
+		return nil
+	}
+	return &PeerInfo{
+		ID:       p.ID.String(),
+		Nickname: p.Nickname,
+		Status:   p.Status,
+		LastSeen: p.LastSeen,
+		Metadata: p.Metadata,
+	}
+}
+
+func convertRoom(r *app.Room) *Room {
+	if r == nil {
+		return nil
+	}
+
+	peers := make([]*PeerInfo, 0, len(r.Peers))
+	for _, p := range r.Peers {
+		peers = append(peers, convertPeerInfo(p))
+	}
+
+	messages := make([]*ChatMessage, len(r.Messages))
+	for i, m := range r.Messages {
+		messages[i] = convertChatMessage(m)
+	}
+
+	return &Room{
+		Name:     r.Name,
+		Peers:    peers,
+		Messages: messages,
+	}
+}
+
+func convertChatMessage(msg *app.ChatMessage) *ChatMessage {
+	if msg == nil {
+		return nil
+	}
+	return &ChatMessage{
+		ID:        msg.ID,
+		From:      msg.From.String(),
+		Nickname:  msg.Nickname,
+		Content:   msg.Content,
+		Timestamp: msg.Timestamp,
+		Type:      MessageType(msg.Type),
+	}
+}
+
+func convertMessageType(mt app.MessageType) MessageType {
+	return MessageType(mt)
+}
+
+func convertEventType(et app.EventType) EventType {
+	return EventType(et)
+}
+
+type Logger interface {
+	LogInfo(message string)
+	LogError(message string)
+}
+
+type defaultLogger struct{}
+
+func (dl *defaultLogger) LogInfo(message string)  {}
+func (dl *defaultLogger) LogError(message string) {}
+
+type EventHandler interface {
+	HandleMessageRecv(*ChatMessage)
+	HandlePeerJoined(*PeerInfo)
+	HandleRoomJoined(*Room)
+}
+
+type defaultEventHandler struct{}
+
+func (h *defaultEventHandler) HandleMessageRecv(msg *ChatMessage) {
+	fmt.Println("message received:")
+	fmt.Println(msg)
+}
+
+func (h *defaultEventHandler) HandlePeerJoined(peerInfo *PeerInfo) {
+	fmt.Println("peer joined:")
+	fmt.Println(peerInfo)
+}
+
+func (h *defaultEventHandler) HandleRoomJoined(room *Room) {
+	fmt.Println("room joined:")
+	fmt.Println(room)
+}
